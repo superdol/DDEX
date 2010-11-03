a05d008b2757cd187afd1e4bee7f26aabd043de4
Chris Masters <chrismasters@gmail.com>
2010-11-03 13:13:13 +0100

    added newsletter and social media links, improved some styling

--- system/application/views/sidebar.php
+++ system/application/views/sidebar.php
@@ -1,21 +1,46 @@
-
-	<div id="standards-documents">
-		<h1>Standards <span class="subtler">&amp;</span> Documents</h1>
-		<p class="notes">NB: You must be a member and <a href="http://example.com/">logged in</a> to download files. <a href="http://example.com/">Become a member</a></p>
-		<div class="document-item first">
-			<h2><a href="http://example.com/" class="pdf-link">ERN Choreography</a></h2>
-			<p><a href="http://example.com/" class="button download">DOWNLOAD NOW</a>11th August 2010<br />3.4mb</p>
-		</div>
-		<div class="document-item">
-			<h2><a href="http://example.com/" class="pdf-link">ERN Choreography</a></h2>
-			<p><a href="http://example.com/" class="button download">DOWNLOAD NOW</a>11th August 2010<br />3.4mb</p>
-		</div>
-		<div class="document-item">
-			<h2><a href="http://example.com/" class="pdf-link">ERN Choreography</a></h2>
-			<p><a href="http://example.com/" class="button download">DOWNLOAD NOW</a>11th August 2010<br />3.4mb</p>
-		</div>
-		<div class="document-item">
-			<h2><a href="http://example.com/" class="pdf-link">ERN Choreography</a></h2>
-			<p><a href="http://example.com/" class="button download">DOWNLOAD NOW</a>11th August 2010<br />3.4mb</p>
-		</div>
+<!-- social media links -->
+<div id="social-media-links">
+	<ul>
+		<li class="facebook"><span class="description">Join us on</span> <a href="#">Facebook</a></li>
+		<li class="twitter"><span class="description">Follow us on</span> <a href="#">Twitter</a></li>
+	</ul>		
+</div>
+<div id="newsletter">
+	<h1 id="newsletter-header">Newsletter</h1>
+	<!-- <form name="newsletter_form" action="/newsletter/api" method="post"> -->
+		<p>
+		  <label for="name">Your name</label>
+		  <input type="text" name="name" id="name"/>
+		</p>
+		<p>
+		  <label for="email">Your email</label>
+		  <input type="text" name="email" id="email" />
+		  <input type="hidden" name="action" value="subscribe" />
+		</p>
+		<p>
+		  <!-- <input type="submit" id="subscribe" value="sign up" /> -->
+		<a href="#" id="subscribe" class="button download">Sign up</a>
+		</p>
+		<div id="newsletter-message"></div>
+	<!-- </form>  -->	
+</div>
+<div id="standards-documents">
+	<h1>Standards <span class="subtler">&amp;</span> Documents</h1>
+	<p class="notes">NB: You must be a member and <a href="http://example.com/">logged in</a> to download files. <a href="http://example.com/">Become a member</a></p>
+	<div class="document-item first">
+		<h2><a href="http://example.com/" class="pdf-link">ERN Choreography</a></h2>
+		<p><a href="http://example.com/" class="button download disabled">DOWNLOAD NOW</a>11th August 2010<br />3.4mb</p>
 	</div>
+	<div class="document-item">
+		<h2><a href="http://example.com/" class="pdf-link">DSR Choreograpy</a></h2>
+		<p><a href="http://example.com/" class="button download disabled">DOWNLOAD NOW</a>6th June 2010<br />8.4mb</p>
+	</div>
+	<div class="document-item">
+		<h2><a href="http://example.com/" class="pdf-link">Automated Message Exchange Protocol</a></h2>
+		<p><a href="http://example.com/" class="button download disabled">DOWNLOAD NOW</a>4th April 2010<br />10.7mb</p>
+	</div>
+	<div class="document-item">
+		<h2><a href="http://example.com/" class="pdf-link">DSR Business Profile Music Rights Societies</a></h2>
+		<p><a href="http://example.com/" class="button download disabled">DOWNLOAD NOW</a>21st January 2010<br />6.3mb</p>
+	</div>
+</div>
